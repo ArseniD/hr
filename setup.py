031693b4da8455a2d96e1c578d9447074ab73ae6
Arseni Dudko <arsenidudko@mail.ru>
2018-03-11 20:10:10 +0300

    Created manifest file, built wheel distribution

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ with open('README.rst', 'r') as f:
 setup(
     name='hr',
     version='0.1.0',
-    description='Python package to manage users on a server based on an “inventory” JSON file.',
+    description='Python package to manage users on a server based on an "inventory" JSON file.',
     long_description=readme,
     author='Arseni Dudko',
     author_email='arseni_dudko@mail.ru',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[]
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'hr=hr.cli:main',
+        ]}
         )
-
--- src/hr/cli.py
+++ src/hr/cli.py
@@ -1,5 +1,7 @@
 import argparse
 
+from setuptools import setup, find_packages
+
 def create_parser():
     parser = argparse.ArgumentParser(description="""
     Manage users premised on json file.
@@ -7,3 +9,15 @@ def create_parser():
     parser.add_argument('path', help='PATH to json inventory file')
     parser.add_argument('--export', action='store_true', help='export current settings to inventory file')
     return parser
+
+
+def main():
+    from hr import users, inventory
+
+    args = create_parser().parse_args()
+
+    if args.export:
+        inventory.dump(args.path)
+    else:
+        users_info = inventory.load(args.path)
+        users.sync(user_info)
